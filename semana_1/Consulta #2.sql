7ef5180db34d7b9895c9fa9ac5eda6eddebb2908
Paulinatello <78061704+Pauu1@users.noreply.github.com>
2021-05-18 22:20:16 -0500

    Add files via upload

---
+++ semana_1/Consulta #2.sql
@@ -0,0 +1,6 @@
+
+usuariosINSERT INTO usuarios 
+VALUES ('1','43','18','Paulina tello','empresaria','2020-05-11 22-10-24'),
+('2','07','53','Eduardo castillo','empresa factible','2020-01-02 11-45-01'),
+('3','12','41','Daniel castellan','especificacion en ventas','2020-12-05 05-34-54');
+
---
+++ semana_1/Consulta #3.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO productos
+VALUES ('1','block','Dimensiones nominales 7 X 40 X 60 CM Dimensiones reales 7 X 40 X 60 CM Área Bruta 0.24 M² Peso por pieza 27 KG','08','50'),
+('2','block','Dimensiones nominales 7 X 40 X 60 CM Dimensiones reales 7 X 40 X 60 CM Área Bruta 0.24 M² Peso por pieza 27 KG','09','100'),
+('3','tabique','Muro de 14 cm. De espesor, de tabique rojo recocido (grueso) asentado con mezcla cemento arena 1:5 acabado común','4','20');
+
